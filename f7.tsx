5902b3324384b3055dfc90580ac7a57805af54cc
Yiyi Sun <yiyisun@gmail.com>
2018-04-11 22:31:12 -0400

    add View, Navbar, Statusbar, Toolbar, Page

--- f7.tsx
+++ f7.tsx
@@ -13,3 +13,37 @@ export default (root, name, id) => new Framework7({
     swipe: 'left',
   }
 });
+
+export const Navbar = ({ title }) => (
+  <div className="navbar">
+    <div className="navbar-inner">
+      <div className="title">{title}</div>
+    </div>
+  </div>
+)
+
+export const Toolbar = (props, children) => (
+  <div className="toolbar">
+    <div className="toolbar-inner">
+      {children || ''}
+    </div>
+  </div>
+)
+
+export const Statusbar = (props, children) => (
+  <div className="statusbar">
+    {children || ''}
+  </div>
+)
+
+export const View = (props, children) => (
+  <div className="view view-main">
+    {children}
+  </div>
+)
+
+export const Page = ({ name }, children) => (
+  <div data-name={name} className="page">
+    {children}
+  </div>
+)
--- main.tsx
+++ main.tsx
@@ -1,41 +1,26 @@
 import app from 'apprun';
-import f7 from './f7';
-import LoginPage from './login';
+import f7, { Navbar, Toolbar, Statusbar, View, Page } from './f7';
+// import LoginPage from './login';
 
-const MainPage = () => <>
+const HomePage = () => <div className="page-content">
   <p>Page content goes here</p>
   <a href="/about/">About app</a>
-</>
+</div>
 
-const Nav = ({ title }) => <>
-  <div className="navbar">
-    <div className="navbar-inner">
-      <div className="title">{title}</div>
-    </div>
-  </div>
-  <div className="toolbar">
-    <div className="toolbar-inner">
-      <a href="#" className="link">Link 1</a>
-      <a href="#" className="link">Link 2</a>
-    </div>
-  </div>
+const view = () => <>
+  <Statusbar />
+  <View>
+    <Page name='home'>
+      <Navbar title='My App' />
+      <Toolbar>
+        <a href="#" className="link">Link 1</a>
+        <a href="#" className="link">Link 2</a>
+      </Toolbar>
+      <HomePage />
+    </Page>
+  </View>
 </>
-
-const App = ({ name, title, Nav, Page }) => <>
-  <div className="statusbar"></div>
-  <div className="view view-main">
-    <div data-name={name} className="page">
-      {Nav ? <Nav title={title} /> : ''}
-      <div className="page-content">
-      <Page />
-      </div>
-    </div>
-  </div>
-</>;
 
 const f7app = f7("#my-app", "My App", "com.mycomp.myapp");
-app.render(
-  document.getElementById('my-app'),
-  <App name="home" title="My App" Nav={Nav} Page={MainPage} />
-  // <App name="login" title="My App" Nav={null} Page={LoginPage}/>
-);
+app.start("my-app", {}, view, {});
+
