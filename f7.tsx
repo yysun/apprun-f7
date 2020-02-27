aa8c24a1566d441fdf88946d7dea54dc9ddb1d5a
Yiyi Sun <yiyisun@gmail.com>
2020-02-26 22:48:21 -0500

    npm upgrade

--- f7.tsx
+++ f7.tsx
@@ -1,69 +1,53 @@
 import app from 'apprun';
 
-export const Navbar = (props, children ) => (
-  <div className="navbar">
-    <div className="navbar-inner">
-      {children || ''}
-    </div>
+export const Navbar = (props, children) => (
+  <div class="navbar">
+    <div class="navbar-inner">{children || ''}</div>
   </div>
-)
+);
 
 export const Toolbar = (props, children) => (
-  <div className="toolbar">
-    <div className="toolbar-inner">
-      {children || ''}
-    </div>
+  <div class="toolbar">
+    <div class="toolbar-inner">{children || ''}</div>
   </div>
-)
+);
 
-export const Statusbar = (props, children) => (
-  <div className="statusbar">
-    {children || ''}
-  </div>
-)
+export const Statusbar = (props, children) => <div class="statusbar">{children || ''}</div>;
 
-export const View = (props, children) => (
-  <div className="view view-main ios-edges">
-    {children}
-  </div>
-)
+export const View = (props, children) => <div class="view view-main ios-edges">{children}</div>;
 
 export const Page = ({ name }, children) => (
-  <div data-name={name} className="page">
+  <div data-name={name} class="page">
     {children}
   </div>
-)
+);
 
 export const LeftPanel = ({ title }, children) => (
-  <div className="panel panel-left panel-reveal">
-    <div className="view view-left">
-      <div className="page">
-        <div className="navbar">
-          <div className="navbar-inner sliding">
-            <div className="title">{title}</div>
+  <div class="panel panel-left panel-reveal panel-resizable panel-init panel-in">
+    <div class="view view-left">
+      <div class="page">
+        <div class="navbar">
+          <div class="navbar-inner sliding">
+            <div class="title">{title}</div>
           </div>
         </div>
-        <div className="page-content">
-          {children}
-        </div>
+        <div class="page-content">{children}</div>
       </div>
     </div>
   </div>
-)
+);
 
 export const RightPanel = ({ title }, children) => (
-  <div className="panel panel-right panel-cover">
-    <div className="view">
-      <div className="page">
-        <div className="navbar">
-          <div className="navbar-inner">
-            <div className="title">{title}</div>
+  <div class="panel panel-right panel-cover">
+    <div class="view">
+      <div class="page">
+        <div class="navbar">
+          <div class="navbar-inner">
+            <div class="title">{title}</div>
           </div>
         </div>
-        <div className="page-content">
-          {children}
-        </div>
+        <div class="page-content">{children}</div>
       </div>
     </div>
   </div>
-)
+);
--- login.tsx
+++ login.tsx
@@ -1,34 +1,41 @@
 import app from 'apprun';
 
-export default () =>
-  <div className="page-content login-screen-content">
-    <div className="login-screen-title">Login</div>
-    <div className="list">
+export default () => (
+  <div class="page-content login-screen-content">
+    <div class="login-screen-title">Login</div>
+    <div class="list">
       <ul>
-        <li className="item-content item-input">
-          <div className="item-inner">
-            <div className="item-title item-label">Username</div>
-            <div className="item-input-wrap">
+        <li class="item-content item-input">
+          <div class="item-inner">
+            <div class="item-title item-label">Username</div>
+            <div class="item-input-wrap">
               <input type="text" name="username" placeholder="Your username" />
             </div>
           </div>
         </li>
-        <li className="item-content item-input">
-          <div className="item-inner">
-            <div className="item-title item-label">Password</div>
-            <div className="item-input-wrap">
+        <li class="item-content item-input">
+          <div class="item-inner">
+            <div class="item-title item-label">Password</div>
+            <div class="item-input-wrap">
               <input type="password" name="password" placeholder="Your password" />
             </div>
           </div>
         </li>
       </ul>
     </div>
-    <div className="list">
+    <div class="list">
       <ul>
         <li>
-          <a href="#" className="item-link list-button login-button">Sign In</a>
+          <a href="#" class="item-link list-button login-button">
+            Sign In
+          </a>
         </li>
       </ul>
-      <div className="block-footer">Some text about login information.<br />Click "Sign In" to close Login Screen</div>
+      <div class="block-footer">
+        Some text about login information.
+        <br />
+        Click "Sign In" to close Login Screen
+      </div>
     </div>
-  </div>;
+  </div>
+);
